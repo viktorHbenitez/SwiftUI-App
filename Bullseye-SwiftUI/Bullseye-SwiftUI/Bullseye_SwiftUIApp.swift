b5dbb57ffd1e1f54ed54b33e5cb9268afaf9a6d5
Victor Hugo Benitez Bosques <victorHbenitez85@hotmail.com>
2022-01-23 13:07:34 -0600

    add project

---
+++ Bullseye-SwiftUI/Bullseye-SwiftUI/Bullseye_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bullseye_SwiftUIApp.swift
+//  Bullseye-SwiftUI
+//
+//  Created by Victor Hugo Benitez Bosques on 23/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Bullseye_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye-SwiftUI/Bullseye-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye-SwiftUI
+//
+//  Created by Victor Hugo Benitez Bosques on 23/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
