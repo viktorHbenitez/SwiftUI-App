76c70988c63ccfd8be6666ffcbdf15cf68312da7
Victor Hugo Benitez Bosques <victorHbenitez85@hotmail.com>
2022-01-23 14:09:55 -0600

    SwiftUI and modifiers

--- Bullseye-SwiftUI/Bullseye-SwiftUI/ContentView.swift
+++ Bullseye-SwiftUI/Bullseye-SwiftUI/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    VStack {
+      Text("😘😘😘 \nPUT THE BULLEYES AS CLSE AS YOU CAN TO")
+        .bold()
+        .kerning(2.0)
+        .multilineTextAlignment(.center)
+        .font(.footnote)
+        .lineSpacing(4.0)
+      Text("89")
+      HStack {
+        Text("1")
+        Slider(value: .constant(50), in: 1.0...100.0)
+        Text("100")
+      }
+      Button(action: {}) {
+        Text("Hit Me")
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+    ContentView()
+      .previewLayout(.fixed(width: 568, height: 320))
+  }
 }
